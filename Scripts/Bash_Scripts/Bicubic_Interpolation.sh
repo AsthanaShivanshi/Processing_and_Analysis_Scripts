e8ac8da793b247c5a44d59639dc0e0f0cc0c62c0
sasthana <shivanshi.asthana@unil.ch>
2025-05-21 18:00:56 +0200

    Changes

--- Scripts/Bash_Scripts/Bicubic_Interpolation.sh
+++ Scripts/Bash_Scripts/Bicubic_Interpolation.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=bicubic
-#SBATCH --output=coarsen_%j.out
-#SBATCH --error=coarsen_%j.err
+#SBATCH --job-name=bicubic_baseline_for_UNet
+#SBATCH --output=bicubic_%j.out
+#SBATCH --error=bicubic_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=3-00:00:00
 
-
-cd "$BASE_DIR"
-
+module load micromamba
+eval "$(micromamba shell hook --shell=bash)"
+source environment.sh
 module load cdo
 
-cdo remapbic,"$GRID_DIR/RhiresD_wet_1971_2023.nc" "$FEATURE_DIR/rhiresd_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_rhiresd.nc"
-cdo remapbic,"$GRID_DIR/TabsD_wet_1971_2023.nc" "$FEATURE_DIR/tabsd_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_tabsd.nc"
-cdo remapbic,"$GRID_DIR/TmaxD_wet_1971_2023.nc" "$FEATURE_DIR/tmaxd_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_tmax.nc"
-cdo remapbic,"$GRID_DIR/TminD_wet_1971_2023.nc" "$FEATURE_DIR/tmind_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_tmind.nc"
+cdo remapbic,"Wetday_files/Targets/RhiresD_wet_1971_2023.nc" "Wetday_files/Features/rhiresd_wet_coarsened.nc" "Wetday_files/Features/bicubic_rhiresd.nc"
+cdo remapbic,"Wetday_files/Targets/TabsD_wet_1971_2023.nc" "Wetday_files/Features/tabsd_wet_coarsened.nc" "Wetday_files/Features/bicubic_tabsd.nc"
+cdo remapbic,"Wetday_files/Targets/TmaxD_wet_1971_2023.nc" "Wetday_files/Features/tmaxd_wet_coarsened.nc" "Wetday_files/Features/bicubic_tmax.nc"
+cdo remapbic,"Wetday_files/Targets/TminD_wet_1971_2023.nc" "Wetday_files/Features/tmind_wet_coarsened.nc" "Wetday_files/Features/bicubic_tmind.nc"
--- Scripts/Bash_Scripts/Bilinear_Interpolation.sh
+++ Scripts/Bash_Scripts/Bilinear_Interpolation.sh
@@ -7,15 +7,14 @@
 #SBATCH --mem=32G
 #SBATCH --time=3-00:00:00
 
-source ../Variable_Config_Scripts/env_filepaths_bilinear.sh
-cd "$BASE_DIR"
+module load micromamba
+eval "$(micromamba shell hook --shell=bash)"
+source environment.sh
 module load cdo
 
-
-# Perform bicubic interpolation using the saved grid files
-cdo remapbil,"$GRID_DIR/RhiresD_wet_1971_2023.nc" "$FEATURE_DIR/rhiresd_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_rhiresd.nc"
-cdo remapbil,"$GRID_DIR/TabsD_wet_1971_2023.nc" "$FEATURE_DIR/tabsd_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_tabsd.nc"
-cdo remapbil,"$GRID_DIR/TmaxD_wet_1971_2023.nc" "$FEATURE_DIR/tmaxd_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_tmax.nc"
-cdo remapbil,"$GRID_DIR/TminD_wet_1971_2023.nc" "$FEATURE_DIR/tmind_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_tmind.nc"
+cdo remapbil,"Wetday_files/Targets/RhiresD_wet_1971_2023.nc" "Wetday_files/Features/rhiresd_wet_coarsened.nc" "Wetday_files/Features_Bilinear/bilinear_rhiresd.nc"
+cdo remapbil,"Wetday_files/Targets/TabsD_wet_1971_2023.nc" "Wetday_files/Features/tabsd_wet_coarsened.nc" "Wetday_files_Bilinear/Features_Bilinear/bilinear_tabsd.nc"
+cdo remapbil,"Wetday_files/Targets/TmaxD_wet_1971_2023.nc" "Wetday_files/Features/tmaxd_wet_coarsened.nc" "Wetday_files_Bilinear/Features_Bilinear/bilinear_tmax.nc"
+cdo remapbil,"Wetday_files/Targets/TminD_wet_1971_2023.nc" "Wetday_files/Features/tmind_wet_coarsened.nc" "Wetday_files_Bilinear/Features_Bilinear/bilinear_tmind.nc"
 
 
