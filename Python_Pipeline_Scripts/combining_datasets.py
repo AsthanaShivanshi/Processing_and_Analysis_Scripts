70e0eb268ee050db703b609f95f3cdde86077336
sasthana <shivanshi.asthana@unil.ch>
2025-06-27 16:35:06 +0200

    Changes

--- Python_Pipeline_Scripts/combining_datasets.py
+++ Python_Pipeline_Scripts/combining_datasets.py
@@ -1,8 +1,16 @@
 import xarray as xr
 from pathlib import Path
 import os
+import sys
 
 BASE_DIR = Path(os.environ["BASE_DIR"])
+
+var_list = ["pr", "tas", "tasmin", "tasmax"]
+if len(sys.argv) > 1:
+    idx = int(sys.argv[1])
+    var_to_process = var_list[idx]
+else:
+    raise ValueError("Index for variable to process: missing (between 0-3)")
 
 var_map_1763 = {"precip": "pr", "temp": "tas", "tmin": "tasmin", "tmax": "tasmax"}
 var_map_1971 = {"RhiresD": "pr", "TabsD": "tas", "TminD": "tasmin", "TmaxD": "tasmax"}
@@ -12,26 +20,41 @@ out_1763 = BASE_DIR / "raw_data" / "Reconstruction_UniBern_1763_2020"
 combined_out = BASE_DIR / "sasthana" / "Downscaling" / "Processing_and_Analysis_Scripts" / "Combined_Dataset"
 combined_out.mkdir(parents=True, exist_ok=True)
 
+std_var=var_to_process
 
-for std_var in ["pr", "tas", "tasmin", "tasmax"]:
-    orig_1763 = [k for k, v in var_map_1763.items() if v == std_var][0]
-    orig_1971 = [k for k, v in var_map_1971.items() if v == std_var][0]
+orig_1763 = [k for k, v in var_map_1763.items() if v == std_var][0]
+orig_1971 = [k for k, v in var_map_1971.items() if v == std_var][0]
 
-    file_1763 = out_1763 / f"{orig_1763}_full.nc"
-    file_1971 = out_1971 / f"{orig_1971}_full.nc"
+file_1763 = out_1763 / f"{orig_1763}_1763_2020.nc"
+file_1971 = out_1971 / f"{orig_1971}_1971_2023.nc"
 
-    # Renaming
-    ds_1763 = xr.open_dataset(file_1763).rename({orig_1763: std_var})
-    ds_1971 = xr.open_dataset(file_1971).rename({orig_1971: std_var})
+    # Rename
+ds_1763 = xr.open_dataset(file_1763).rename({orig_1763: std_var})
+ds_1971 = xr.open_dataset(file_1971).rename({orig_1971: std_var})
 
-    # Adding 'source' dimension
-    ds_1763 = ds_1763.expand_dims(source=["pretrain"])
-    ds_1971 = ds_1971.expand_dims(source=["train"])
+print(f"{std_var} 1763 dims: {ds_1763.dims}")
+print(f"{std_var} 1971 dims: {ds_1971.dims}")
 
-    # Concatenating along 'source'
-    ds_combined = xr.concat([ds_1763, ds_1971], dim="source")
-    print(f"Combined ds created for {std_var}")
+# 'source' dim
+ds_1763 = ds_1763.expand_dims(source=["pretrain"])
+ds_1971 = ds_1971.expand_dims(source=["train"])
 
-    ds_combined.to_netcdf(combined_out / f"{std_var}_combined.nc")
+
+#For handling E/N and lat/lon problem
+for coord in ["lat", "lon"]:
+    if coord in ds_1763.coords:
+        ds_1763 = ds_1763.drop_vars(coord)
+    if coord in ds_1971.coords:
+        ds_1971 = ds_1971.drop_vars(coord)
+
+print(f"{std_var} 1763 dims: {ds_1763.dims}")
+print(f"{std_var} 1971 dims: {ds_1971.dims}")
+
+    # Concatenating along source due to overlapping time coords after 1971
+ds_combined = xr.concat([ds_1763, ds_1971], dim="source")
+print(f"Combined ds created for {std_var}")
+print(f"{std_var} combined dimensions: {ds_combined.dims}")
+
+ds_combined.to_netcdf(combined_out / f"{std_var}_combined.nc")
 
 print("Combined files with source dimension saved.")
--- combine_datasets.sh
+++ combine_datasets.sh
@@ -5,15 +5,11 @@
 #SBATCH --error=sasthana/Downscaling/Processing_and_Analysis_Scripts/logs/combine_datasets_processing_%A_%a.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4       
-#SBATCH --mem=256G
+#SBATCH --mem=500G
 #SBATCH --time=3-00:00:00
-#SBATCH --array=0-3%1   
+#SBATCH --array 0-3
 
-
-source sasthana/Downscaling/Processing_and_Analysis_Scripts/environment.sh 
+source sasthana/Downscaling/Processing_and_Analysis_Scripts/environment.sh
 export PROJ_LIB="$ENVIRONMENT/share/proj"
-export HDF5_USE_FILE_LOCKING=FALSE 
 
-cd "$BASE_DIR" || { echo "[ERROR] Failed to cd into BASE_DIR"; exit 1; }
-
-python Python_Pipeline_Scripts/combining_datasets.py
+python /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Processing_and_Analysis_Scripts/Python_Pipeline_Scripts/combining_datasets.py $SLURM_ARRAY_TASK_ID
