8bcd7d3ba906ccf5bbb39f5327737f570c611222
sasthana <shivanshi.asthana@unil.ch>
2025-07-14 14:50:51 +0200

    Changes

--- combine_datasets.sh
+++ combine_datasets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=tmin_tmax_combination
+#SBATCH --job-name=combine_tmin
 #SBATCH --chdir=/work/FAC/FGSE/IDYST/tbeucler/downscaling
-#SBATCH --output=sasthana/Downscaling/Processing_and_Analysis_Scripts/logs/combine_tmin_tmax_%j.log
-#SBATCH --error=sasthana/Downscaling/Processing_and_Analysis_Scripts/logs/combine_tmin_tmax_%j.log
+#SBATCH --output=sasthana/Downscaling/Processing_and_Analysis_Scripts/logs/combine_tmin_%j.log
+#SBATCH --error=sasthana/Downscaling/Processing_and_Analysis_Scripts/logs/combine_tmin_%j.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4       
 #SBATCH --mem=500G
@@ -11,7 +11,7 @@
 source sasthana/Downscaling/Processing_and_Analysis_Scripts/environment.sh
 export PROJ_LIB="$ENVIRONMENT/share/proj"
 
-for idx in 2 3; do 
+for idx in 2; do 
     echo "Processing variable index $idx"
     python /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Processing_and_Analysis_Scripts/Python_Pipeline_Scripts/combining_datasets.py $idx
 done
