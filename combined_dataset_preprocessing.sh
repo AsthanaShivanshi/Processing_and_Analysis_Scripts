49b81f4ca3aeefc4129699b0b0e288e107484882
sasthana <shivanshi.asthana@unil.ch>
2025-07-14 15:16:38 +0200

    Changes

--- combined_dataset_preprocessing.sh
+++ combined_dataset_preprocessing.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=500G
 #SBATCH --time=3-00:00:00
-#SBATCH --array=0-1
+#SBATCH --array=2-3
 
 source sasthana/Downscaling/Processing_and_Analysis_Scripts/environment.sh
 module load cdo
