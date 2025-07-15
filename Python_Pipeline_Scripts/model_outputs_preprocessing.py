c157fb9ff2f086aa121ff84a506794c1e4cefefd
sasthana <shivanshi.asthana@unil.ch>
2025-07-15 19:28:08 +0200

    Changes

--- Python_Pipeline_Scripts/model_outputs_preprocessing.py
+++ Python_Pipeline_Scripts/model_outputs_preprocessing.py
@@ -1,60 +1,58 @@
-#!/usr/bin/env python3
 import os
 import subprocess
 import config
 import xarray as xr
 
-CH_BOX = (5.8, 10.6, 45.7, 47.9)
+CH_BOX = (5, 11, 45, 48)
 
 def process_file(source, target, outname, oldvar, newvar):
+    outdir = os.path.dirname(outname)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir, exist_ok=True)
+
+    tmpfile = outname.replace(".nc", "_step1.nc")
+
     cdo_cmd = [
         "cdo",
         f"sellonlatbox,{CH_BOX[0]},{CH_BOX[1]},{CH_BOX[2]},{CH_BOX[3]}",
         f"-remapbic,{target}",
         source,
-        outname
+        tmpfile
     ]
     print("Running:", " ".join(cdo_cmd))
-    subprocess.run(cdo_cmd, check=True)
-    print(f"Remapped and cropped: {outname}")
-
-    if os.path.exists(outname):
-        try:
-            os.remove(outname)
-        except PermissionError:
-            print(f"Permission denied: cannot remove {outname}")
-            return
-
-    ds = xr.open_dataset(outname)
+    result = subprocess.run(cdo_cmd, capture_output=True, text=True)
+    ds = xr.open_dataset(tmpfile)
     ds = ds.rename({oldvar: newvar})
     ds.to_netcdf(outname, mode="w")
     ds.close()
     print(f"Renamed {oldvar} to {newvar} in {outname}")
 
+    os.remove(tmpfile)
+    print(f"Deleted tempfile {tmpfile}")
 
 pairs = [
     (
-        f"{config.MODELS_DIR}/pr_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r1i1p1_rcp85_1971-2099.nc",
+        f"{config.MODELS_DIR}/pr_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r2i1p1_rcp85_1971-2099.nc",
         f"{config.DATASETS_PRETRAINING_DIR}/precip_step2_coarse.nc",
-        f"{config.MODELS_DIR}/pr_r01_coarse.nc",
+        f"{config.MODELS_DIR}/precip_r02_coarse.nc",
         "pr", "precip"
     ),
     (
-        f"{config.MODELS_DIR}/tas_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r1i1p1_rcp85_1971-2099.nc",
+        f"{config.MODELS_DIR}/tas_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r2i1p1_rcp85_1971-2099.nc",
         f"{config.DATASETS_PRETRAINING_DIR}/temp_step2_coarse.nc",
-        f"{config.MODELS_DIR}/tas_r01_coarse.nc",
+        f"{config.MODELS_DIR}/temp_r02_coarse.nc",
         "tas", "temp"
     ),
     (
-        f"{config.MODELS_DIR}/tasmax_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r1i1p1_rcp85_1971-2099.nc",
+        f"{config.MODELS_DIR}/tasmax_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r2i1p1_rcp85_1971-2099.nc",
         f"{config.DATASETS_PRETRAINING_DIR}/tmax_step2_coarse.nc",
-        f"{config.MODELS_DIR}/tmax_r01_coarse.nc",
+        f"{config.MODELS_DIR}/tmax_r02_coarse.nc",
         "tasmax", "tmax"
     ),
     (
-        f"{config.MODELS_DIR}/tasmin_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r1i1p1_rcp85_1971-2099.nc",
+        f"{config.MODELS_DIR}/tasmin_day_EUR-11_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_r2i1p1_rcp85_1971-2099.nc",
         f"{config.DATASETS_PRETRAINING_DIR}/tmin_step2_coarse.nc",
-        f"{config.MODELS_DIR}/tmin_r01_coarse.nc",
+        f"{config.MODELS_DIR}/tmin_r02_coarse.nc",
         "tasmin", "tmin"
     )
 ]
