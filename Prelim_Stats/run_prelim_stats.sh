61bf46f0b6a8679af29d6a5cf35b38667cabc4f6
sasthana <shivanshi.asthana@unil.ch>
2025-08-24 13:12:49 +0200

    Changes

--- Prelim_Stats/run_prelim_stats.sh
+++ Prelim_Stats/run_prelim_stats.sh
@@ -6,9 +6,11 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=512G
 #SBATCH --time=16:00:00
+#SBATCH --array=0-3
 
 
 module load python
 source environment.sh
 
-python Prelim_Stats/Mean_Annual_Cycle_Comparison.py #--var $SLURM_ARRAY_TASK_ID
+python Prelim_Stats/Mean_Annual_Cycle_Comparison.py --var $SLURM_ARRAY_TASK_ID --city "Zurich" --lat 47.3769 --lon 8.5417
+
