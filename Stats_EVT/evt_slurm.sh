b209e894d157cb983bb0a13ebe9408ac14a5dc51
sasthana <shivanshi.asthana@unil.ch>
2025-10-02 15:02:20 +0200

    Changes

--- Stats_EVT/evt_slurm.sh
+++ Stats_EVT/evt_slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=EVT_runner
-#SBATCH --output=logs/EVT_return_bm%j.out
-#SBATCH --error=logs/EVT_return_bm%j.err
+#SBATCH --output=logs/EVT_runner_%j.out
+#SBATCH --error=logs/EVT_runner_%j.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=512G
-#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=500G
+#SBATCH --time=10:00:00
 
-
+module load micromamba
+eval "$(micromamba shell hook --shell=bash)"
 source environment.sh
-module load python
 
 python Stats_EVT/Return_Levels_gridded_BM.py
